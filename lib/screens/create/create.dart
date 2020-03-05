dc815a55e09acac2029be0e526f4b3f01d34758b
Subwires <admin@subwires.com>
2020-03-05 23:40:58 +0000

    :construction: New screens

---
+++ lib/screens/create/create.dart
@@ -0,0 +1,30 @@
+// Dependencies
+import 'package:flutter/material.dart';
+// Services
+import 'package:storytap/services/auth.dart';
+// Shared
+import 'package:storytap/shared/provider.dart';
+// Screens
+import 'package:storytap/screens/authenticate/authenticate.dart';
+
+class Create extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
+
+    return Container(
+        height: _height,
+        width: _width,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("Create",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 24,
+              )),
+        ),
+    );
+  }
+}
