db9c84ab094a5a5224b66d64e7e42dc176a63a41
Subwires <admin@subwires.com>
2020-05-01 15:56:40 +0100

    :card_file_box: create pages and branches

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -3,27 +3,28 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 // Models
 import 'package:storytap/models/book.dart';
 import 'package:storytap/models/page.dart';
-
-// *** 
+import 'package:storytap/models/branch.dart';
+// ***
 // Manages cloud firestore functionalities using DatabaseService.
 
 class DatabaseService {
-  
   String uid;
 
   DatabaseService({this.uid});
 
-
   // A collection reference to the "users" collection
-  final CollectionReference usersCollection = Firestore.instance.collection("users");
+  final CollectionReference usersCollection =
+      Firestore.instance.collection("users");
   // A collection reference to the "books" collection
-  final CollectionReference booksCollection = Firestore.instance.collection("books");
+  final CollectionReference booksCollection =
+      Firestore.instance.collection("books");
 
   Future createBookDocument(
-    // Creates a new book document in the usersCollection and also creates a new collection "books" separately
-    Book book, Page page
-  ) async {
-    DocumentReference bookid = await usersCollection.document(uid).collection("books").add({
+      // Creates a new book document in the usersCollection and also creates a new collection "books" separately
+      Book book,
+      Page page) async {
+    DocumentReference bookid =
+        await usersCollection.document(uid).collection("books").add({
       'bookTitle': book.title,
       'bookDescription': book.description,
       'bookGenre': book.genre,
@@ -33,13 +34,14 @@ class DatabaseService {
       'isComplete': false,
     });
     await createPageDocument(bookid.documentID, book, page);
-    await createBookCollection(book, bookid.documentID, uid);
+    await createBookCollection(
+        book, bookid.documentID, uid); // Other bookCollection
     // createPageDocument(bookid, pageTitle, pageText, lastUpdated)
   }
 
-  // Creates a bookCollection which references the user that created that book and allows faster querying
+  // Creates a separate bookCollection which references the user that created that book and allows faster querying (less reads usage)
   Future createBookCollection(Book book, String bookid, String uid) async {
-    await booksCollection.document(bookid).setData({ 
+    await booksCollection.document(bookid).setData({
       'bookTitle': book.title,
       'bookDescription': book.description,
       'bookGenre': book.genre,
@@ -47,19 +49,43 @@ class DatabaseService {
       'bookCreationDate': book.creationDate,
       'bookLastUpdated': book.lastUpdated,
       'isComplete': false,
-      'author' : uid,});
+      'author': uid,
+    });
   }
 
   // Creates a page document for a particular book
   Future createPageDocument(String bookid, Book book, Page page) async {
-        return await usersCollection.document(uid).collection("books").document(bookid).collection("pages").add({
-          // 'book' : book,
-          'pageTitle' : page.title,
-          'pageText' : page.text,
-          'pageLastUpdated' : page.lastUpdated,
-          'initial' : page.initial,
-        });
-      }
+    return await usersCollection
+        .document(uid)
+        .collection("books")
+        .document(bookid)
+        .collection("pages")
+        .add({
+      // 'book' : book,
+      'pageTitle': page.title,
+      'pageText': page.text,
+      'pageLastUpdated': page.lastUpdated,
+      'initial': page.initial,
+    });
+  }
+
+  // Creates a branch document for a particular page
+  Future createBranchDocument(String bookid, String pageid, Book book,
+      Page page, Branch branch) async {
+      return await usersCollection
+          .document(uid)
+          .collection("books")
+          .document(bookid)
+          .collection("pages")
+          .document(pageid)
+          .collection("branches")
+          .add({
+        'branchNumber': branch.number,
+        'branchText': branch.text,
+        'branchPageReference': branch.pageID,
+        'branchPageTitle': branch.pageTitle,
+      });
+    }
 
   Stream<QuerySnapshot> getUserBookStreamSnapshots() async* {
     yield* usersCollection.document(uid).collection("books").snapshots();
@@ -71,8 +97,7 @@ class DatabaseService {
     yield* booksCollection.snapshots();
   }
 
-  Future getUserID() async{
+  Future getUserID() async {
     return uid;
   }
-
 }
